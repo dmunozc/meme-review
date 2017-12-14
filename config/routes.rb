f65e3a1928de2c4132fc216604114176ce4675dd
David Muno <dmunoz11@georgefox.edu>
2017-12-13 21:57:51 -0800

    Finished with devise and some materialize

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'signup', password: 'forgot', confirmation: 'confirmation', unlock: 'unlock'}
-  as :user do
-    get 'login', to: 'devise/sessions#new'
-    get 'signup', to: 'devise/registrations#new'
-    get 'forgot', to: 'devise/passwords#new'
-    get 'confirmation', to: 'devise/confirmations#new'
-  end
+  devise_for :users
   root 'static_pages#home'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
